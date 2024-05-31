7b3e2f00dd7bd6a45f413e7c46106a0f20dc0db6
Mike Beck <mike@mikeybeck.com>
2024-05-31 12:11:16 +1200

    Add caching and client info

--- background.js
+++ background.js
@@ -10,22 +10,52 @@ async function fetchAndParseRSS() {
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.message === "parsedXML") {
-    items = request.items;
-    console.log(items);
+    chrome.storage.local.get(["items", "expiryDate"], function (data) {
+      if (data.expiryDate && new Date().getTime() > data.expiryDate) {
+        // The items have expired, clear them
+        console.log('clearing');
+        chrome.storage.local.remove(["items", "expiryDate"]);
+      }
+
+      if (data.items) {
+        console.log('not clearing');
+        // The items have not expired, use them
+        const oldTitles = data.items.map((item) => item.title);
+        const newTitles = request.items.map((item) => item.title);
+        if (JSON.stringify(oldTitles) !== JSON.stringify(newTitles)) {
+          items = request.items;
+          const expiryDate = new Date();
+          expiryDate.setHours(expiryDate.getHours() + 24); // Set expiry date to 24 hours from now
+          chrome.storage.local.set({
+            items: items,
+            expiryDate: expiryDate.getTime(),
+          });
+        }
+      } else {
+        console.log('setting items');
+        items = request.items;
+        const expiryDate = new Date();
+        expiryDate.setHours(expiryDate.getHours() + 24); // Set expiry date to 24 hours from now
+        chrome.storage.local.set({
+          items: items,
+          expiryDate: expiryDate.getTime(),
+        });
+      }
+    });
   }
 });
 
 // async function fetchAndParseRSS() {
 //   const response = await fetch(
-    // "https://www.upwork.com/ab/feed/jobs/rss?amount=500-999%2C1000-4999%2C5000-&contractor_tier=2%2C3&hourly_rate=60-&location=Oceania&paging=NaN-undefined&q=wordpress&sort=recency&t=0%2C1&api_params=1&securityToken=1469d0c33c7d72f34a75a3e9d644d8963f02a90b0a3144af2f60af2fb96b2ed35873080daabe9b435f94498f6bdf2bd06bdb90a2d887e28501bd5e79734116dc&userUid=424233588756824064&orgUid=424233588761018369"
+// "https://www.upwork.com/ab/feed/jobs/rss?amount=500-999%2C1000-4999%2C5000-&contractor_tier=2%2C3&hourly_rate=60-&location=Oceania&paging=NaN-undefined&q=wordpress&sort=recency&t=0%2C1&api_params=1&securityToken=1469d0c33c7d72f34a75a3e9d644d8963f02a90b0a3144af2f60af2fb96b2ed35873080daabe9b435f94498f6bdf2bd06bdb90a2d887e28501bd5e79734116dc&userUid=424233588756824064&orgUid=424233588761018369"
 //   );
 //   const text = await response.text();
 //   const parser = new DOMParser();
 //   const doc = parser.parseFromString(text, "application/xml");
 //   items = Array.from(doc.querySelectorAll("item")).map((item) => ({
-    // title: item.querySelector("title").textContent,
-    // link: item.querySelector("link").textContent,
-    // description: item.querySelector("description").textContent,
+// title: item.querySelector("title").textContent,
+// link: item.querySelector("link").textContent,
+// description: item.querySelector("description").textContent,
 //   }));
 // }
 
@@ -35,11 +65,12 @@ chrome.runtime.onMessageExternal.addListener(function (
   sendResponse
 ) {
   if (request === "getItems") {
-    sendResponse(items);
+    chrome.storage.local.get("items", function (data) {
+      sendResponse(data.items);
+    });
     return true;
   }
 });
-
 // chrome.action.onClicked.addListener(async (tab) => {
 //   await fetchAndParseRSS();
 // });
@@ -62,4 +93,19 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.message === "buttonClicked") {
     fetchAndParseRSS();
   }
+
+  if (request.message === "clearCache") {
+    clearCache();
+  }
 });
+
+function clearCache() {
+  chrome.storage.local.clear(function () {
+    var error = chrome.runtime.lastError;
+    if (error) {
+      console.error(error);
+    } else {
+      console.log("Cache cleared");
+    }
+  });
+}
--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-chrome.runtime.sendMessage("getItems", function (response) {
+chrome.runtime.sendMessage("getItems", async function (response) {
   if (chrome.runtime.lastError) {
     console.error(chrome.runtime.lastError.message);
     return;
@@ -7,22 +7,43 @@ chrome.runtime.sendMessage("getItems", function (response) {
   const items = response;
   const itemsContainer = document.getElementById("items");
   console.log(itemsContainer);
-  items.forEach((item) => {
+  for (const item of items) {
     const itemElement = document.createElement("div");
+    const clientData = await fetchAndParseHTML(item.link);
+
+    clientData.hourlyRate = parseFloat(clientData.hourlyRate.replace('$', ''));
+
+    if (clientData.hourlyRate < 20) {
+        continue;
+    } else if (clientData.hourlyRate < 40 || isNaN(clientData.hourlyRate)) {
+        itemElement.style.backgroundColor = 'red';
+    } else if (clientData.hourlyRate < 50) {
+        itemElement.style.backgroundColor = 'yellow';
+    } else {
+        itemElement.style.backgroundColor = 'green';
+    }
+
     itemElement.innerHTML = `
       <h2><a href="${item.link}" target="_blank">${item.title}</a></h2>
       <p>${item.description}</p>
+      <p>Hourly rate: ${clientData.hourlyRate}</p>
+      <p>Rating: ${clientData.rating}</p>
+      <p>Hire rate: ${clientData.hireRate}%</p>
+      <p>Last viewed: ${clientData.lastViewed}</p>
     `;
     itemsContainer.appendChild(itemElement);
-  });
+  }
 });
 
 document.getElementById("yourButtonId").addEventListener("click", function () {
   chrome.runtime.sendMessage({ message: "buttonClicked" });
 });
+document.getElementById("clearCacheButton").addEventListener("click", function () {
+  chrome.runtime.sendMessage({ message: "clearCache" });
+});
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.message === 'parseXML') {
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.message === "parseXML") {
     const parser = new DOMParser();
     const doc = parser.parseFromString(request.text, "application/xml");
     const items = Array.from(doc.querySelectorAll("item")).map((item) => ({
@@ -30,6 +51,68 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
       link: item.querySelector("link").textContent,
       description: item.querySelector("description").textContent,
     }));
-    chrome.runtime.sendMessage({message: 'parsedXML', items: items});
+    chrome.runtime.sendMessage({ message: "parsedXML", items: items });
   }
 });
+
+async function fetchAndParseHTML(url) {
+  const response = await fetch(url);
+  const text = await response.text();
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(text, "text/html");
+
+  let clientData = {
+    'hourlyRate': getHourlyRate(doc),
+    'rating': getRating(doc),
+    'hireRate': getHireRate(doc),
+    'lastViewed': getLastViewed(doc)
+  }
+
+  return clientData;
+}
+
+function getHourlyRate(doc) {
+    const element = doc.querySelector("[data-qa='client-hourly-rate']");
+    let hourlyRate = '';
+    if (element) {
+        const regex = /\$[\d.]+/g;
+        const match = regex.exec(element.textContent);
+        hourlyRate = match ? match[0] : null;
+    }
+
+    return hourlyRate;
+}
+
+function getRating(doc) {
+    const element = doc.querySelector(".air3-rating-value-text");
+    return element ? element.textContent : null;
+}
+
+function getHireRate(doc) {
+    // Element looks like <li data-qa="client-job-posting-stats" data-v-b14c8560=""><strong data-v-b14c8560=""> 98 jobs posted </strong> <div data-v-b14c8560=""> 2% hire rate, 1 open job </div></li>
+    const element = doc.querySelector("[data-qa='client-job-posting-stats']");
+    if (!element) {
+        console.log('No hiring rate element found');
+        return null;
+    }
+
+    const hireRateRegex = /(\d+)% hire rate/;
+    const hireRateMatch = hireRateRegex.exec(element.textContent);
+    const hireRate = hireRateMatch ? hireRateMatch[1] : null;
+    return hireRate;
+}
+
+function getLastViewed(doc) {
+    const element = doc.querySelector(".client-activity-items");
+    if (!element) {
+        console.log('No activity items element found');
+        return null;
+    }
+
+    // Get last viewed by client.  Element looks like <span class="value" data-v-8d65ed88="">1 hour ago</span>
+    const lastViewedRegex = /(\d+) (hour|day|hours|days) ago/;
+    const lastViewedMatch = lastViewedRegex.exec(element.textContent);
+    const lastViewed = lastViewedMatch ? lastViewedMatch[1] : null;
+
+    return lastViewed;
+}
